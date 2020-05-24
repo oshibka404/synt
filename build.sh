93fa3d06fbe38358a1b1e900511f85d4433bb70e
Andrey <andrey@ozorn.in>
2020-05-24 18:57:58 +0200

    Refactor build script: use subshell, use '-target' option of faust2api

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-rm -rf temp
-mkdir temp
-cp faust/* temp/
-cd temp
-faust2api -ios -nozip main.dsp
-rm -rf ../ios/Runner/DSP
-mkdir ../ios/Runner/DSP
-mv ./DspFaust.cpp ../ios/Runner/DSP
-mv ./DspFaust.h ../ios/Runner/DSP
-mv ./README.md ../ios/Runner/DSP
-cd ..
-rm -rf temp
-
+(
+    cd faust;
+    faust2api -ios -nozip -target ../ios/Runner/DSP main.dsp
+)
