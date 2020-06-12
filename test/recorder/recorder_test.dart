151de9f031eb973c0d2d57793406c298509218d5
Andrey <andrey@ozorn.in>
2020-06-12 19:52:59 +0200

    Unit test for recorder's bypass

--- test/recorder/recorder_test.dart
+++ test/recorder/recorder_test.dart
@@ -1,17 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:perfect_first_synth/controller/keyboard/keyboard_action.dart';
 import 'package:perfect_first_synth/recorder/recorder.dart';
 import 'package:perfect_first_synth/recorder/state.dart';
 
 void main() {
   group('Recorder state: ', () {
-    test('Initial state is "playing"', () async {
+    test('Initial state is "playing"', () {
       var recorder = Recorder(input: Stream.empty());
       recorder.stateStream.last.then((value) {
         expect(value, RecorderState.playing);
       });
     });
     
-    test('Sets ready to recording state', () async {
+    test('Sets ready to recording state', () {
       var recorder = Recorder(input: Stream.empty());
       recorder.setReadyToRec();
       expect(recorder.stateStream, emitsInOrder([
@@ -20,7 +23,7 @@ void main() {
       ]));
     });
 
-    test('Sets recording state', () async {
+    test('Sets recording state', () {
       var recorder = Recorder(input: Stream.empty());
       recorder.startRec(Offset(0, 0));
       expect(recorder.stateStream, emitsInOrder([
@@ -29,7 +32,7 @@ void main() {
       ]));
     });
 
-    test('Goes to playing state after being stopped', () async {
+    test('Goes to playing state after being stopped', () {
       var recorder = Recorder(input: Stream.empty());
       recorder.setReadyToRec();
       recorder.startRec(Offset(0, 0));
@@ -40,6 +43,24 @@ void main() {
         RecorderState.recording,
         RecorderState.playing,
       ]));
+    });
+  });
+  
+  group('Recording: ', () {
+
+    test('Bypasses all signals in correct order', () {
+      var actions = [
+        KeyboardAction(time: DateTime.now(), type: KeyboardActionType.start, voiceId: 1),
+        KeyboardAction(time: DateTime.now(), type: KeyboardActionType.modify, voiceId: 1),
+        KeyboardAction(time: DateTime.now(), type: KeyboardActionType.start, voiceId: 2),
+      ];
+      var controller = StreamController<KeyboardAction>();
+      var recorder = Recorder(input: controller.stream);
+      actions.forEach((element) {
+        controller.add(element);
+      });
+      expect(recorder.output, emitsInOrder(actions));
+      controller.close();
     });
   });
 }
