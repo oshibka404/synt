a4e7eb6ebbcae0dd09d33850f734b013298c44f7
Andrey <andrey@ozorn.in>
2020-06-17 00:04:11 +0200

    Running wave

--- lib/controller/keyboard/keyboard_painter.dart
+++ lib/controller/keyboard/keyboard_painter.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'pointer_data.dart';
@@ -19,7 +21,7 @@ class KeyboardPainter extends CustomPainter {
 
   Canvas canvas;
 
-  Color backgroundColor = Colors.white;
+  Color backgroundColor = Colors.grey[850];
 
   bool isTonic(int stepNumber) {
     return stepNumber % 7 == 0;
@@ -27,12 +29,146 @@ class KeyboardPainter extends CustomPainter {
 
   getXPositionOfKey(int keyNumber) => keyNumber * pixelsPerStep + pixelsPerStep / 2;
 
+  // TODO: refactor this nightmare!
+  Path getWavePath(double x, int waves) {
+    var phaseShift = (DateTime.now().millisecondsSinceEpoch % 1000) / 1000; // from 0 to 1
+    var path = Path();
+    double amplitude = pixelsPerStep / 4;
+    var availableHeight = size.height - (2 * padding);
+    var waveLength = availableHeight / waves;
+
+    var valueAt0 = sin((1 - phaseShift) * 2*pi);
+
+    // full cycle is 1, peaks on .25 and .75
+    path.moveTo(x + valueAt0 * amplitude, padding);
+
+    if (phaseShift > .75) {
+      var extremum1 = phaseShift - .75;
+      var extremum2 = phaseShift - .25;
+      var zero = phaseShift - .5;
+
+      path.quadraticBezierTo(
+        (x + amplitude), padding + extremum1 * waveLength,
+        x, padding + waveLength * zero
+      );
+      path.quadraticBezierTo(
+        (x - amplitude), padding + extremum2 * waveLength,
+        x, padding + phaseShift * waveLength
+      );
+    } else if (phaseShift > .5) {
+      var zero = phaseShift - .5;
+      var point1 = zero / 2;
+      var extremum2 = phaseShift - .25;
+      
+      path.quadraticBezierTo(
+        (x + sin(zero/2 * 2*pi) * amplitude), padding + point1 * waveLength,
+        x, padding + waveLength * zero
+      );
+      path.quadraticBezierTo(
+        (x - amplitude), padding + extremum2 * waveLength,
+        x, padding + phaseShift * waveLength
+      );
+    } else if (phaseShift > .25) {
+      var extremum = phaseShift - .25;
+      
+      path.quadraticBezierTo(
+        (x - amplitude), padding + extremum * waveLength,
+        x, padding + phaseShift * waveLength
+      );
+    } else {
+      path.quadraticBezierTo(
+        (x + sin((phaseShift - 1) / 2 * 2*pi) * amplitude), padding + phaseShift / 2 * waveLength,
+        x, padding + phaseShift * waveLength
+      );
+    }
+
+    for (int i = 0; i < waves - 1; i++) {
+      double waveStartY = padding + waveLength * (i + phaseShift);
+      path.quadraticBezierTo(
+        (x + amplitude), waveStartY + waveLength / 4,
+        x, waveStartY + waveLength / 2
+      );
+      path.quadraticBezierTo(
+        (x - amplitude), waveStartY + waveLength * 3 / 4,
+        x, waveStartY + waveLength
+      );
+    }
+
+    var tail = size.height - padding;
+
+    path.moveTo(x + valueAt0 * amplitude, tail);
+    
+    var phaseUnShift = 1 - phaseShift;
+    if (phaseUnShift > .75) {
+      var extremum1 = phaseUnShift - .75;
+      var extremum2 = phaseUnShift - .25;
+      var zero = phaseUnShift - .5;
+
+      path.quadraticBezierTo(
+        (x - amplitude), tail - extremum1 * waveLength,
+        x, tail - waveLength * zero
+      );
+      path.quadraticBezierTo(
+        (x + amplitude), tail - extremum2 * waveLength,
+        x, tail - phaseUnShift * waveLength
+      );
+    } else if (phaseUnShift > .5) {
+      var zero = phaseUnShift - .5;
+      var point1 = zero / 2;
+      var extremum2 = phaseUnShift - .25;
+      
+      path.quadraticBezierTo(
+        (x - sin(zero/2 * 2*pi) * amplitude), tail - point1 * waveLength,
+        x, tail - waveLength * zero
+      );
+      path.quadraticBezierTo(
+        (x + amplitude), tail - extremum2 * waveLength,
+        x, tail - phaseUnShift * waveLength
+      );
+    } else if (phaseUnShift > .25) {
+      var extremum = phaseUnShift - .25;
+      
+      path.quadraticBezierTo(
+        (x + amplitude), tail - extremum * waveLength,
+        x, tail - phaseUnShift * waveLength
+      );
+    } else {
+      path.quadraticBezierTo(
+        (x - sin((phaseUnShift - 1) / 2 * 2*pi) * amplitude), tail - phaseUnShift / 2 * waveLength,
+        x, tail - phaseUnShift * waveLength
+      );
+    }
+
+    return path;
+  }
+
+  void drawPressedKey(double x, PointerData pressingPointer) {
+    var keyColor = Color.lerp(
+      mainColor,
+      invert(mainColor),
+      pressingPointer.position.dy / size.height
+    );
+    var paint = Paint()
+      ..color = keyColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0;
+    
+    var path = getWavePath(x, 3);
+    canvas.drawPath(path, paint);
+  }
+
   void drawKey(
     int keyNumber,
     Map<int, PointerData> pointers,
   ) {
     double x = getXPositionOfKey(keyNumber);
-    final Color keyColor = getKeyColor(keyNumber, pointers);
+    PointerData pressingPointer = pointers.values.firstWhere((pointerData) {
+      return pointerData.position.dx ~/ pixelsPerStep == keyNumber;
+    }, orElse: () => null);
+    
+    if (pressingPointer != null) {
+      drawPressedKey(x, pressingPointer);
+    } else {
       canvas.drawRRect(
         RRect.fromRectAndRadius(
           Rect.fromPoints(
@@ -41,20 +177,14 @@ class KeyboardPainter extends CustomPainter {
           ),
           Radius.circular(2)
         ),
-        Paint()..color = keyColor
+        Paint()..color = getKeyColor(keyNumber, pointers)
       );
+    }
   }
   
   final double pixelsPerStep;
 
   Color getKeyColor(int keyNumber, Map<int, PointerData> pointers) {
-    PointerData pointer = pointers.values.firstWhere((pointerData) {
-      return pointerData.position.dx ~/ pixelsPerStep == keyNumber;
-    }, orElse: () => null);
-
-    if (pointer != null) {
-      return Color.lerp(mainColor, invert(mainColor), pointer.position.dy / size.height);
-    }
     if (isTonic(keyNumber)) {
       return Colors.amber;
     }
