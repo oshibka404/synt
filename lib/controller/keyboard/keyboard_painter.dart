a96af36a634d880f1451b7c9362c35573b04562c
Andrey <andrey@ozorn.in>
2020-06-16 19:07:39 +0200

    Highlight active key

--- lib/controller/keyboard/keyboard_painter.dart
+++ lib/controller/keyboard/keyboard_painter.dart
@@ -13,48 +13,81 @@ class KeyboardPainter extends CustomPainter {
 
   final Color mainColor;
 
+  final double padding = 30;
+
   bool isTonic(int stepNumber) {
     return stepNumber % 7 == 0;
+  }
+
+  getXPositionOfKey(int keyNumber) => keyNumber * pixelsPerStep + pixelsPerStep / 2;
+
+  void drawKey({
+    Canvas canvas,
+    int keyNumber,
+    Map<int, PointerData> pointers,
+    Iterable<int> pressedKeys,
+    double length,
+  }) {
+    double x = getXPositionOfKey(keyNumber);
+    final Color keyColor = getKeyColor(keyNumber, pressedKeys);
+      canvas.drawRRect(
+        RRect.fromRectAndRadius(
+          Rect.fromPoints(
+            Offset(x - 2, padding),
+            Offset(x + 2, length + padding)
+          ),
+          Radius.circular(2)
+        ),
+        Paint()..color = keyColor
+      );
   }
   
   final double pixelsPerStep;
 
-  Color getKeyColor(int keyNumber) {
+  Color getKeyColor(int keyNumber, Iterable<int> pressedKeys) {
+    if (pressedKeys.any((pressedKey) => pressedKey == keyNumber)) {
+      return Colors.deepPurple;
+    }
     if (isTonic(keyNumber)) {
       return Colors.amber;
     }
     return mainColor;
   }
 
+  int getClosestStepNumber(Offset position) {
+    return position.dx ~/ pixelsPerStep;
+  }
+
+  double getXPositionOfClosestKey(Offset pointerPosition) {
+    return pixelsPerStep * getClosestStepNumber(pointerPosition) + pixelsPerStep / 2;
+  }
+
   @override
   void paint(Canvas canvas, Size size) {
-    double keyPosition = pixelsPerStep / 2;
-    int keyNumber = 0;
+    int keysOnScreen = size.width ~/ pixelsPerStep;
 
     canvas.drawRect(
       Rect.fromLTWH(0, 0, size.width, size.height),
       Paint()..color = Colors.white
     );
 
-    while (keyPosition < size.width) {
-      final Color keyColor = getKeyColor(keyNumber);
-      canvas.drawRRect(
-        RRect.fromRectAndRadius(
-          Rect.fromPoints(
-            Offset(keyPosition - 2, 30),
-            Offset(keyPosition + 2, size.height - 30)
-          ),
-          Radius.circular(2)
-        ),
-        Paint()..color = keyColor
+    Iterable<int> pressedKeys = pointers.values.map((pointerData) {
+      return getClosestStepNumber(pointerData.position);
+    });
+
+    for (int key = 0; key < keysOnScreen; key++) {
+      drawKey(
+        canvas: canvas,
+        keyNumber: key,
+        pointers: pointers,
+        pressedKeys: pressedKeys,
+        length: size.height - (padding * 2),
       );
-      keyPosition += pixelsPerStep;
-      keyNumber++;
     }
     
     pointers.forEach((_, pointer) {
       canvas.drawCircle(
-        pointer.position,
+        Offset(getXPositionOfClosestKey(pointer.position), pointer.position.dy),
         pointer.pressure * 50,
         Paint()..color = mainColor.withOpacity(.5)
       );
