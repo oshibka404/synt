2fb19b98a365853be518f32f71eef901df10a8e2
Andrey <andrey@ozorn.in>
2020-09-20 16:16:55 +0200

    rename filename getter method

--- lib/controller/settings/config.dart
+++ lib/controller/settings/config.dart
@@ -16,19 +16,19 @@ abstract class Config {
     return directory.path;
   }
 
-  Future<File> get _globalSettingsFile async {
+  Future<File> get _settingsFile async {
     final path = await _localPath;
     return File('$path/$fileName');
   }
 
   void save() async {
-    final file = await _globalSettingsFile;
+    final file = await _settingsFile;
     var encodedSettings = jsonEncode(this.toJson());
     file.writeAsString(encodedSettings);
   }
 
   Future<void> load() async {
-    final file = await _globalSettingsFile;
+    final file = await _settingsFile;
     if (await file.exists()) {
       String contents = await file.readAsString();
 
